865ccbf2b32bf71b882dbde87cfb92dcac3b1ceb
Andrea <andrea@MacBook-Pro-de-Andrea.local>
2021-05-30 21:44:12 -0300

    Change language

--- cotizaciones/src/App.js
+++ cotizaciones/src/App.js
@@ -4,35 +4,30 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      cotizaciones: [],
+      exchangeRate: [],
       loaded: false
     }
+    this.renderExchange = this.renderExchange.bind(this)
   }
 
   componentDidMount() {
-    this.callApi()
-      .then(body => {
-        console.log(body)
-        this.setState({ cotizaciones: body.data, loaded: true })
-      })
-      .catch(err => console.log(err));
+    this.getExchangeRate();
   }
 
-  callApi = async () => {
-    var response = await fetch('/cotizaciones/USD');
+  getExchangeRate = async () => {
+    this.setState({exchangeRate: [], loaded: false });
+    let response = await fetch('/exchange');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    return body;
+    this.setState({ exchangeRate: body.data, loaded: true })
   }
 
-  refresh(){
-    this.setState({ cotizaciones: [], loaded: false});
-    this.componentDidMount();
+  renderExchange = () => {
+
   }
 
   render() {
-    var cotizacionesList = this.state.cotizaciones.map((cot, i) =>
-    {
+    let exchangeRates = this.state.exchangeRate.map((cot, i) => {
         return (
           <div className="card" id="caja" key={i}>
             <div className="card-header">
@@ -43,20 +38,33 @@ class App extends Component {
             </div>
           </div>
         )
-      }
-    );
+    });
 
-    var loading = (<div id="posicion-spinner"><i className="fa fa-spinner fa-spin fa-10x"></i></div>);
+    const spinner = (<div id="posicion-spinner"><i className="fa fa-spinner fa-spin fa-10x"></i></div>);
 
     return  (
       <div className = "App">
         <div className="container">
-          <h3 className="jumbotron jumbotron-fluid text-center prueba" id="titular">Cotización de Divisas Extranjeras</h3>
+          <h3 className="jumbotron jumbotron-fluid text-center" id="titular">Exchange rates</h3>
           <div>
-              <button className="btn btn-primary btn-lg btn-block" id="boton" onClick={()=> this.refresh()}> Refresh </button>
+              <button className="btn btn-primary btn-lg btn-block" id="boton" onClick={this.getExchangeRate}> Refresh </button>
           </div>
           <div>
-            { this.state.loaded ? cotizacionesList : loading}
+            {this.state.loaded && (
+              this.state.exchangeRate.map((cot, i) => {
+                  return (
+                    <div className="card" id="caja" key={i}>
+                      <div className="card-header">
+                          <p className="font-weight-bold">{cot.result.source}</p>
+                      </div>
+                      <div className="card-body">
+                          <p><span className="font-italic">ARS</span> {cot.result.value.toFixed(5)}</p>
+                      </div>
+                    </div>
+                  )
+              })
+            )}
+            {!this.state.loadad && spinner}
           </div>
         </div>
       </div>
